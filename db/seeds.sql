668727249d137508585126b5b943a6189db30b89
Tri Pham <tjpham25@gmail.com>
2018-12-06 16:12:13 -0800

    Hope

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,14 +3,14 @@ burger_name,
 devoured
 ) VALUES
 (
-"Beef Double Cheese Burger",
+"Double Double",
 0
 ),
 (
-"Patty Melt Burger",
+"Cheeseburger",
 0
 ),
 (
-"Mushroom & Swiss Double Cheese Burger",
+"Hamburger",
 0
 );
