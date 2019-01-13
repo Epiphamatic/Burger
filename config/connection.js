af95bf8ff2f583f07a77fe6f7a22b675fb4912ca
Tri Pham <tjpham25@gmail.com>
2019-01-12 22:18:36 -0800

    Yes

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+var connection;
+if (process.env.JAWSDB_URL)
+{
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
 connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
@@ -8,6 +14,7 @@ connection = mysql.createConnection({
   password: "",
   database: "burgers_db"
 });
+}
 
 connection.connect(function(err) {
   if (err) {
